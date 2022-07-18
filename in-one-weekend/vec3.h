516e5fa020cf2768c6108b45a1e76820336e3490
10ge6 <dsf@jugemu.systems>
2022-07-18 17:16:57 -0300

    fix typos and re-render

--- in-one-weekend/vec3.h
+++ in-one-weekend/vec3.h
@@ -4,8 +4,6 @@
 #include <cmath>
 #include <iostream>
 
-using std::sqrt;
-
 class vec3 {
         public:
                 // constructor member initializer lists
@@ -17,7 +15,7 @@ class vec3 {
                 double z() const { return e[2]; }
 
                 // operator overloading
-                vec3() operator - () const { return vec3(-e[0], -e[1], -e[2]); }
+                vec3 operator - () const { return vec3(-e[0], -e[1], -e[2]); }
                 double operator [] (int i) const { return e[i]; }                 // subscript operator
                 double& operator [] (int i) { return e[i]; }
 
@@ -40,7 +38,7 @@ class vec3 {
                 }
 
                 double length() const {
-                        return sqrt(length_squared());
+                        return std::sqrt(length_squared());
                 }
 
                 double length_squared() const {
@@ -55,12 +53,10 @@ class vec3 {
 using point3 = vec3;            // 3D point
 using color = vec3;             // RGB value
 
-#endif
-
 // utility functions
 
 inline std::ostream& operator << (std::ostream &out, const vec3 &v) {
-        return out << v.e[0] << ' ' << v.e[1] >> ' ' << v.e[2];
+        return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
 inline vec3 operator + (const vec3 &u, const vec3 &v) {
@@ -68,7 +64,7 @@ inline vec3 operator + (const vec3 &u, const vec3 &v) {
 }
 
 inline vec3 operator - (const vec3 &u, const vec3 &v) {
-        return vec3(u.e[0] - v.e[0], )
+        return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline vec3 operator * (const vec3 &u, const vec3 &v) {
@@ -102,3 +98,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
         return v / v.length();
 }
+
+#endif
